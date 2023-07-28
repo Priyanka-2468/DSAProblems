00e0057bb162e48c88d3259092b27b6afd2a0c82
Priyanka <priyankagorai123@gmail.com>
2023-07-29 02:01:58 +0530

    first commit

---
+++ maxProduct.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+      int n=nums.size();
+      int leftProduct=1;
+      int rightProduct=1;
+      int ans=nums[0];
+      for(int i=0;i<n;i++){
+          leftProduct=leftProduct==0?1:leftProduct;
+          rightProduct=rightProduct==0?1:rightProduct;
+          leftProduct*=nums[i];
+          rightProduct*=nums[n-1-i];
+          ans=max(ans,max(leftProduct,rightProduct));
+
+      }  
+      return ans;
+    }
+};
